cc0f74dd56940545db1ad03c61bc55d9441aa644
Vishal Pandey <111230926+pandevis@users.noreply.github.com>
2024-05-02 08:40:11 +0000

    Added hello API

--- src/main/java/com/myapp/helloworld/HelloWorldApplication.java
+++ src/main/java/com/myapp/helloworld/HelloWorldApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String getHello(){
+		return "Hello Vishal Pandey !!!";
 	}
 
 }
